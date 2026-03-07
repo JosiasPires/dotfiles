0613bb3953ab81a361edfc71c10ffb0c242950ff
JosiasPires <104743492+JosiasPires@users.noreply.github.com>
2026-03-07 00:44:20 -0300

    Add lazyvim to neovim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,5 +1,2 @@
-vim.opt.termguicolors = true
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE"})
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE"})
-
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
